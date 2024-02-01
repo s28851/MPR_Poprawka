ece68bbad8bd7ea7ad71923c5ee659e759362c8a
Marek <md@mediman.pl>
2024-02-01 19:21:20 +0100

    sdfsfd

---
+++ src/main/java/shopee_s28851/Product.java
@@ -0,0 +1,36 @@
+package shopee_s28851;
+
+public class Product {
+    private String ProductName;
+    private double ProductPrize;
+    private int ProductNumber;
+
+
+
+    public Product(String ProductName, double ProductPrize, int ProductNumber) {
+        this.ProductName = ProductName;
+        this.ProductPrize = ProductPrize;
+        this.ProductNumber = ProductNumber);
+
+
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public int getProductPrize() {
+        return ProductPrize;
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public int getProductNumber() {
+        return ProductNumber;
+    }
+    public void setProductNumber(int productnumber){
+        this.ProductNumber = productnumber;
+    }
+}
