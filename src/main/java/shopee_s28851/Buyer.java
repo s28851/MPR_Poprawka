a42f9280542c3daeabd82471888b2496d59ab5ba
Marek <md@mediman.pl>
2024-02-01 19:38:18 +0100

    sdfsfgrghtg

---
+++ src/main/java/shopee_s28851/Buyer.java
@@ -0,0 +1,17 @@
+package shopee_s28851;
+
+public class Buyer {
+    public int id;
+
+    public Buyer(int ID) {
+        this.id = ID;
+    }
+
+    public void setID(int ID) {
+        this.id = ID;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
